b55eaf289b219fd4def545ed56b445b1940b2611
Aeltumn <daniel@goossens.ch>
2025-03-18 00:10:38 +0100

    Implement basic items/locations for the layout

--- worlds/rayman2/__init__.py
+++ worlds/rayman2/__init__.py
@@ -1,10 +1,17 @@
 from typing import Type, ClassVar
 
-from BaseClasses import Tutorial
+from BaseClasses import Tutorial, ItemClassification, Item, Region, LocationProgressType, Location
 from Options import PerGameCommonOptions
 from worlds.AutoWorld import WebWorld, World
+from .Layout import levels
 from .Options import create_option_groups, Rayman2Options
 
+
+class Rayman2Item(Item):
+    game: str = "Rayman 2"
+
+class Rayman2Location(Location):
+    game: str = "Rayman 2"
 
 class Rayman2Web(WebWorld):
     option_groups = create_option_groups()
@@ -26,8 +33,113 @@ class Rayman2World(World):
 
     game = "Rayman 2"
     web = Rayman2Web()
+    topology_present = False
+
     item_name_to_id = {}
     location_name_to_id = {}
 
     options_dataclass: ClassVar[Type[PerGameCommonOptions]] = Rayman2Options
     options: Rayman2Options
+
+    def __init__(self, multiworld, player):
+        super(Rayman2World, self).__init__(multiworld, player)
+
+        # Initialize initial values
+        self.base_id = 1651615
+
+        # Store variables with the level shuffle and lum gates
+        self.levelSwaps = {}
+        self.lumGates = {}
+        self.item_pool = []
+
+    def create_regions(self) -> None:
+        # Go through all levels in the level layout and create item/location objects,
+        # every location is an item and every item is a location in Rayman 2.
+
+        # This is because the game engine has a set id for every single item and all
+        # items are tracked individually as completion, so we want to properly track
+        # which specific cage or super lum was picked up. For now we use the internal
+        # engine names for them all so they are easily distinguished in the game which
+        # can be changed later.
+
+        # Start by creating the menu
+        menu = Region("Menu", self.player, self.multiworld)
+        self.multiworld.regions.append(menu)
+
+        lumTally = 0
+        silverLumTally = 0
+        for levelName, levelInfo in levels:
+            # TODO Check for lum gates
+
+            lastRegion = menu
+            for subLevelName, subLevelInfo in levelInfo.sublevels:
+                # TODO Randomly swap around sub levels based on whether we need a silver
+
+                # TODO Add access rules for masks to enter the Pirate Ship
+
+                # Create this level and connect it
+                region = Region(subLevelName, self.player, self.multiworld)
+                self.multiworld.regions.append(menu)
+                lastRegion.connect(region)
+                lastRegion = region
+
+                lumTally += subLevelInfo.regularLums
+                silverLumTally += subLevelInfo.silverLums
+
+                # Create checks for all super lums
+                for superLum in subLevelInfo.superLums:
+                    self.create(region, superLum, ItemClassification.filler, LocationProgressType.DEFAULT)
+
+                # Create checks for all cages
+                for cage, lumsInside in subLevelInfo.cages:
+                    self.create(region, cage, ItemClassification.useful, LocationProgressType.PRIORITY)
+                    lumTally += lumsInside
+
+                # Create checks for all special checks
+                for specialItem in subLevelInfo.special:
+                    self.create(region, specialItem, ItemClassification.progression, LocationProgressType.PRIORITY)
+
+            # Connect the last sub-level back to the menu
+            if lastRegion.name != menu.name:
+                lastRegion.connect(menu)
+
+            # TODO Process the extra level
+
+    def create_item(self, item: str,
+                    classification: ItemClassification = ItemClassification.progression) -> Rayman2Item:
+        return Rayman2Item(item, classification, self.item_name_to_id[item], self.player)
+
+    def create_items(self):
+        self.multiworld.itempool += self.item_pool
+
+    # Creates items and locations for every input
+    def create(self, region, name, classification: ItemClassification, progression: LocationProgressType):
+        id = self.claim_id()
+
+        # Add the item to the mappings
+        self.item_name_to_id[name] = id
+        self.item_id_to_name[id] = name
+        self.location_name_to_id[name] = id
+        self.location_id_to_name[id] = name
+
+        # Add the item to the item pool
+        self.item_pool.append(self.create_item(name, classification))
+
+        # Add the item to the region
+        location = Rayman2Location(self.player, name, id, region)
+        location.progression_type = progression
+        region.locations.append(location)
+
+    # Claim a new id for an item and location
+    def claim_id(self):
+        result = self.base_id
+        self.base_id = result + 1
+        return result
+
+    # Include level swaps and lum gate information in the slot data
+    # sent to the game on connection
+    def fill_slot_data(self):
+        slot_data = {}
+        slot_data["level_swaps"] = self.levelSwaps
+        slot_data["lum_gates"] = self.lumGates
+        return slot_data
