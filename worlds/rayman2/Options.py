68870971bb274a17461232996ab115d489484803
Aeltumn <daniel@goossens.ch>
2026-05-02 15:48:03 +0200

    Tweaked defaults so high numbers still generate fine and small tweaks

--- worlds/rayman2/Options.py
+++ worlds/rayman2/Options.py
@@ -41,7 +41,7 @@ class FirstGateRequirement(Range):
     """The amount of lums required to enter the Sanctuary of Water and Ice."""
     display_name = "First Lum Gate Requirement"
     range_start = 0
-    range_end = 250
+    range_end = 200
     default = 100
 
 
@@ -49,7 +49,7 @@ class SecondGateRequirement(Range):
     """The amount of lums required to enter the Sanctuary of Stone & Fire."""
     display_name = "Second Lum Gate Requirement"
     range_start = 0
-    range_end = 500
+    range_end = 450
     default = 300
 
 
@@ -57,7 +57,7 @@ class ThirdGateRequirement(Range):
     """The amount of lums required to enter Beneath the Sanctuary of Rock & Lava."""
     display_name = "Third Lum Gate Requirement"
     range_start = 0
-    range_end = 700
+    range_end = 600
     default = 450
 
 
@@ -65,7 +65,7 @@ class FourthGateRequirement(Range):
     """The amount of lums required to enter the Iron Mountains."""
     display_name = "Fourth Lum Gate Requirement"
     range_start = 0
-    range_end = 850
+    range_end = 800
     default = 550
 
 
@@ -73,7 +73,7 @@ class WalkOfLifeRequirement(Range):
     """The amount of lums required to enter the Walk of Life."""
     display_name = "Walk of Life Lum Requirement"
     range_start = 0
-    range_end = 300
+    range_end = 750
     default = 60
 
 
@@ -81,7 +81,7 @@ class WalkOfPowerRequirement(Range):
     """The amount of lums required to enter the Walk of Power."""
     display_name = "Walk of Power Lum Requirement"
     range_start = 0
-    range_end = 900
+    range_end = 750
     default = 475
 
 
